86a9d4d8a9153518caf90a25e6f4ae7df0fc5f4d
Alexander <jma5645@my.utexas.edu>
2025-05-22 15:04:12 -0500

    Add analysistable.R

---
+++ r/analysistable.R
@@ -0,0 +1,36 @@
+analysistable <- function(modelsum, contrasts, tables, tabnames, outpath){
+  
+  if(length(tables)!=length(tabnames)){
+    stop("ERROR: List of tables and vector of table numbers must be of the same length.")
+  }
+  
+  wb <- openxlsx::createWorkbook()
+  
+  addWorksheet(wb, sheetName="model-summary")
+  openxlsx::writeData(wb, sheet="model-summary", modelsum$coefficients, rowNames=TRUE)
+  
+  for(i in 1:length(contrasts)){
+    em <- unlist(contrasts[i])
+    preds <- em$emmeans@roles$predictors
+    if(length(preds)==1){
+      name <- paste("emmeans-", preds[1], sep="")
+    } else if(length(preds)==2){
+      name <- paste("emmeans-", preds[1], "|", preds[2], sep="")
+    } else if(length(preds)==3){
+      name <- paste("emmeans-", preds[1], "|", preds[2], "|", preds[3], sep="")
+    } else {
+      stop("ERROR: Function only handles up to three predictors.")
+    }
+    addWorksheet(wb, sheetName=name)
+    openxlsx::writeData(wb, sheet=name, em$contrasts, rowNames=TRUE)
+  }
+  
+  for(j in 1:length(tables)){
+    t <- tables[j][[1]]
+    addWorksheet(wb, sheetName=tabnames[j])
+    openxlsx::writeData(wb, sheet=tabnames[j], t)
+  }
+  
+  openxlsx::saveWorkbook(wb, paste(outpath, "_", today, ".xlsx", sep="", collapse=NULL))
+
+}
